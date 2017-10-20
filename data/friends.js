7c0587b6fe97a05cf88def2bdaaca242a56751d6
Curtis0210 <curtis0210@gmail.com>
2017-10-19 23:11:08 -0700

    new

---
+++ data/friends.js
@@ -0,0 +1,16 @@
+var friends = [ {
+	"name" : "",
+	"photo" : "",
+	"scores" : [5,5,2,3,1,5,2,1,3,5]
+}, {
+	"name" : "",
+	"photo" : "",
+	"scores" : [1,5,1,1,1,1,1,1,5,5]
+
+}, {
+	"name" : "",
+	"photo" : "",
+	"scores" : [5,3,2,3,2,5,1,3,3,4]
+}];
+
+module.exports = friends;
